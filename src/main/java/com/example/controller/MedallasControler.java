b5982d85821de7068aa4e1fe53f63f9f559989f1
josemanriquejulialopez <josepthebest@gmail.com>
2016-12-07 20:40:05 +0100

    Commit

--- src/main/java/com/example/controller/MedallasControler.java
+++ src/main/java/com/example/controller/MedallasControler.java
@@ -1,9 +1,38 @@
 package com.example.controller;
 
-/**
- * Created by dam on 7/12/16.
- */
+import com.example.domain.Medallas;
+import com.example.repository.MedallasRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/medals")
 public class MedallasControler {
+    @Autowired
+    private MedallasRepository medallasRepository;
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Medallas createMedal(@RequestBody Medallas medallas){
+        return medallasRepository.save(medallas);
+    }
 
+    @GetMapping
+    public List<Medallas> getAllMedals(){
+        return medallasRepository.findAll();
+    }
+
+    @PutMapping
+    public Medallas updateMedal(@RequestBody Medallas medallas){
+        return medallasRepository.save(medallas);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteMedalID(@PathVariable Long id){
+        Medallas medallas = medallasRepository.findOne(id);
+        if(medallas != null ) medallasRepository.delete(id);
+    }
 }
